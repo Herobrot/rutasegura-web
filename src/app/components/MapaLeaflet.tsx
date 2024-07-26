97b8d6ca177f28c748c9f202eef85f3a3dbb1761
Herobroto <herobrotelpro@gmail.com>
2024-07-26 11:58:27 -0600

    Ajuste de estilo en mapcontainer

--- src/app/components/MapaLeaflet.tsx
+++ src/app/components/MapaLeaflet.tsx
@@ -171,6 +171,7 @@ const LeafletMap: React.FC<LeafletMapProps> = ({ position }) => {
           <MapContainer
             center={position}
             zoom={13}
+            style={{ height: '100%', width: '100%' }}
           >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
